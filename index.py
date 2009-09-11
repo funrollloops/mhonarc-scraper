55537e6679d15efbe8b7d6077f429ea0df404559
Sagar Mittal <sagar@sagarmittal.com>
2009-09-11 00:52:10 -0700

    Split list update, creation, and viewing into separate pages/methods

--- index.py
+++ index.py
@@ -2,18 +2,11 @@ from google.appengine.api import users
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api.labs.taskqueue import Task
 from models import Message, List, Thread
 from email_loader import strip_tags
 from datetime import datetime, timedelta
 import re
-
-def is_logged_in(request_handler):
-  user = users.get_current_user()
-  if user:
-    request_handler.user = user
-    return true
-  else:
-    request_handler.redirect(users.create_login_url(request_handler.request.uri))
 
 def gql_limit1(model,  **conditions):
   query = "WHERE " + " AND ".join("%s = :%s" % (k, k) for k in conditions)
@@ -22,7 +15,15 @@ def gql_limit1(model,  **conditions):
     return result
   return None
 
-def update_list(list_url, start_msg, limit = 10):
+def get_list_from_url(list_url):
+  list = gql_limit1(List, list_url = list_url)
+  if list is None:
+      if re.match(r'^http(s?)://.*/msg(\d{5}).html$', list_url):
+        list_url = list_url[:-len('/msg00000.html')]
+        list = gql_limit1(List, list_url = list_url)
+  return (list, list_url)
+
+def update_list(list, start_msg, limit = 10):
   import email_loader
   import urllib
   thread_id_cache = {}
@@ -30,7 +31,7 @@ def update_list(list_url, start_msg, limit = 10):
   message_pool = []
   new = 0
   for i in range(start_msg, start_msg + limit):
-    url = "%s/msg%05i.html" % (list_url, i)
+    url = "%s/msg%05i.html" % (list.list_url, i)
     result = email_loader.parser(urllib.urlopen(url))
     result['source_url'] = url
     result['list_msg_id'] = i
@@ -54,7 +55,7 @@ def update_list(list_url, start_msg, limit = 10):
     if 'thread_id' not in result:
       result['thread_id'] = result['message_id']
       thread = Thread(thread_id = result['message_id'],
-                      list_url = list_url,
+                      list_url = list.list_url,
                       last_message_time = result['date'],
                       subject = result['subject'],
                       last_message_body = result['body'],
@@ -83,51 +84,43 @@ def update_list(list_url, start_msg, limit = 10):
     message.put()
   for thread_id, thread in thread_pool.items():
     thread.put()
+  if new > 0:
+    list.num_fetched_msg += new
+    list.last_fetched_time = datetime.now()
+    list.put()
   return new
+
+def schedule_list_update(list):
+  Task(url = '/list/update', params = {'list' : list.list_url},
+       countdown = 1).add()
 
 def render(rh, path, **kw):
   rh.response.out.write(template.render(path, kw))
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    user = users.get_current_user()
-    if user:
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, ' + user.nickname())
-    else:
-      self.redirect(users.create_login_url(self.request.uri))
-    
+    lists = List.all().order('-last_fetched_time')
+    render(self, 'index.html', lists = lists)
+
 class ShowList(webapp.RequestHandler):
   def get(self):
-    list_url = self.request.get('list')
-    # Check to see that URL is of the right form.
-
-    list = gql_limit1(List, list_url = list_url)
+    list, list_url = get_list_from_url(self.request.get('list'))
+    msg = None
     if list is None:
-      if not re.match(r'^http(s?)://.*/msg(\d{5}).html$', list_url):
-        render(self, 'error.html',
-               msg = "Given url does not end with msgXXXXX.html")
-        return
-      list_url = list_url[:-len('/msg00000.html')]
-      list = gql_limit1(List, list_url = list_url)
-
-    if list is None:
-      new_msgs = update_list(list_url, 0)
-      if new_msgs > 0:
-        list = List(list_url = list_url, num_fetched_msg = new_msgs,
-                    last_fetched_time = datetime.now())
-        list.put()
-      else:
-        render(self, 'error.html',
-               msg = "Unable to get any messages from url \"%s\"" % list_url)
-        return
+      render(self, 'error.html', msg = 'The specified list does not exist.')
+      return
+    # If the last time the list was updated was over 12 hours ago, schedule an
+    # update
+    if list.last_fetched_time < datetime.now() - timedelta(hours=12):
+      schedule_list_update(list)
+      msg = 'Fetching new messages...'
 
     threads = Thread.all().filter('list_url =', list_url).\
                      order('-last_message_time')
     formatted_threads = []
     for t in threads:
       if len(t.participants) > 3:
-        t.participants = [t.participants[:1], '...'] + t.participants[-2:]
+        t.participants = [t.participants[0], '...'] + t.participants[-2:]
       t.participants = [', '.join(p.split()[0] for p in  t.participants)]
       t.subject = strip_tags(t.subject)
       t.last_message_body = strip_tags(t.last_message_body)[:100]
@@ -140,7 +133,44 @@ class ShowList(webapp.RequestHandler):
         t.short_date = datetime.strftime(t.last_message_time, '%D')
       formatted_threads.append(t)
 
-    render(self, 'view.html', list_url = list_url, threads = formatted_threads)
+    render(self, 'view.html', list = list, threads = formatted_threads,
+                              msg = msg)
+
+class CreateList(webapp.RequestHandler):
+  def post(self):
+    list, list_url = get_list_from_url(self.request.get('list'))
+
+    if list is not None:
+      render(self, 'error.html', msg = 'That list already exists!')
+
+    list = List(list_url = list_url, num_fetched_msg = 0,
+                last_fetched_time = datetime.now())
+
+    if update_list(list, 0, 1) == 0:
+      render(self, 'error.html',
+      msg = "Unable to get any messages from url \"%s\"" % list_url)
+      return
+
+    schedule_list_update(list)
+    render(self, 'info.html', msg = 'List added; messages are being fetched.')
+    
+
+class UpdateList(webapp.RequestHandler):
+  def do(self):
+    list = gql_limit1(List, list_url = self.request.get('list'))
+    if not list:
+      render(self, 'error.html', msg='unknown list')
+    elif update_list(list, list.num_fetched_msg, limit = 1) == 1:
+      render(self, 'info.html', msg='one message added, update scheduled')
+      schedule_list_update(list)
+    else:
+      render(self, 'info.html', msg='no messages added')
+
+  def post(self):
+    self.do()
+
+  def get(self):
+    self.do()
     
 class ShowThread(webapp.RequestHandler):
   def get(self):
@@ -165,8 +195,10 @@ class ShowThread(webapp.RequestHandler):
 
 application = webapp.WSGIApplication([
                                       ('/', MainPage),
-                                      ('/list', ShowList),
-                                      ('/thread', ShowThread)
+                                      ('/list/create', CreateList),
+                                      ('/list/show', ShowList),
+                                      ('/list/update', UpdateList),
+                                      ('/thread/show', ShowThread),
                                      ],
                                      debug=True)
 
